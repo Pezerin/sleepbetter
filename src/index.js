634b34f76b7228a02e645644acd4dcd290f58d6c
Mikul Shah <134720598+Pezerin@users.noreply.github.com>
2025-05-07 20:46:53 -0400

    Created chat messaging functions and basic layout for application

--- src/index.js
+++ src/index.js
@@ -1,1 +1,7 @@
 import "./styles.css";
+import { sendMessage } from "./response";
+
+console.log(await sendMessage("Hi, I'm 17 and can't sleep well."));
+console.log(
+  await sendMessage("I go to sleep at 10, wake up at 7, and avoid caffeine.")
+);
